864f4ce34a07c1e181c8e2e2080a9146a62b07b0
Aryan Gupta <kuantum.freak@gmail.com>
2018-06-21 23:12:50 -0400

    Finish iBus module

--- iBUS/iBUS.ino
+++ iBUS/iBUS.ino
@@ -7,34 +7,15 @@ void setup() {
 void loop() {
 	auto st = micros();
 	
-	rdata_idx = 0;
-	UCSR0B |= (1 << RXCIE0); // Enable Rx Complete Interrupt
-	while (UCSR0B & (1 << RXCIE0));
-	rdata_idx = 0;
+	get_recv_data();
+	
 	uint32_t len = micros() - st;
 	
 	Serial.print(len);
 	Serial.print('\t');
-	
-	recv[ 0] = raw[ 0] + (raw[ 1] << 8);
-	recv[ 1] = raw[ 2] + (raw[ 3] << 8);
-	recv[ 2] = raw[ 4] + (raw[ 5] << 8);
-	recv[ 3] = raw[ 6] + (raw[ 7] << 8);
-	recv[ 4] = raw[ 8] + (raw[ 9] << 8);
-	recv[ 5] = raw[10] + (raw[11] << 8);
-	recv[ 6] = raw[12] + (raw[13] << 8);
-	recv[ 7] = raw[14] + (raw[15] << 8);
-	recv[ 8] = raw[16] + (raw[17] << 8);
-	recv[ 9] = raw[18] + (raw[19] << 8);
-	recv[10] = raw[20] + (raw[21] << 8);
-	recv[11] = raw[22] + (raw[23] << 8); // 1. dummy channels
-	recv[12] = raw[24] + (raw[25] << 8); // 2. 
-	recv[13] = raw[26] + (raw[27] << 8); // 3. 
-	recv[14] = raw[28] + (raw[29] << 8); // 4. 
-	recv[15] = raw[30] + (raw[31] << 8); 
-	
-	for (uint8_t i = 0; i < 32; i++) { 
-		Serial.print(raw[i]);
+
+	for (uint8_t i = 0; i < 15; i++) { 
+		Serial.print(recv[i]);
 		Serial.print(' ');
 	}
 	Serial.println(' ');
--- iBUS/iBus.h
+++ iBUS/iBus.h
@@ -1,58 +1,17 @@
 #pragma once
 
-#ifndef BAUD_TOL
-#   define BAUD_TOL 2
-#endif
-
-uint16_t recv[17];
-volatile uint8_t raw[34];
-
-volatile uint8_t rdata_idx = 0;
-
-ISR(USART_RX_vect) {
-	uint8_t data = UDR0;
-	if (rdata_idx == 0 and data != 0x20) {
-		return;
-	}
-	
-	raw[rdata_idx] = data;
-	++rdata_idx;
-	
-	if (rdata_idx == 32) {
-		UCSR0B &= ~(1 << RXCIE0); // Enable Rx Complete Interrupt
-	}
-}
+uint16_t recv[15];
 
 void setup_recv() {
-	unsigned long baud = 115200;
-	
-	uint8_t use2x = 0;
-	uint16_t ubbr =  (F_CPU + 8UL * baud) / (16UL * baud) - 1UL;
-	if ( (100 * (F_CPU)) > (16 * (ubbr + 1) * (100 * ubbr + ubbr * BAUD_TOL)) ) {
-		use2x = 1;
-		ubbr = (F_CPU + 4UL * baud) / (8UL * baud) - 1UL;
-	}
-
-	UBRR0L = ubbr & 0xff;
-	UBRR0H = ubbr >> 8;
-	if (use2x) {
-		UCSR0A |= (1 << U2X0);
-	} else {
-		UCSR0A &= ~(1 << U2X0);
-	}
-	
-	UCSR0B |= (1<<RXEN0);  // Enable Reciever
-	UCSR0B |= (1<<RXCIE0); // Enable Rx Complete Interrupt
-	
-	Serial.begin(baud);
+	Serial.begin(115200);
 }
 
 /*
 	The general iBus protocol is this:
-	iBus is a serial protocol that operates at 115.2 kHz.
+	iBus is a Serial protocol that operates at 115.2 kHz.
 	The first two bytes are sync/start bytes consisting off
-	0x20 then 0x40. Then there are 14 channels of data, each
-	channel being 16bits. The data is little endian so bytes
+	0x20 then 0x40. Then there are 14 channels of recv, each
+	channel being 16bits. The recv is little endian so bytes
 	must be swapped. After that, there are 2 bytes that are 
 	check sum bytes and is just 0xFFFF subtracted by every byte
 	we transfered. (I dont know what this means but I bet that 
@@ -61,56 +20,52 @@ void setup_recv() {
 	
 	and this:
 	
-	Only 10 channels of the data really means somthing to us so
+	Only 10 channels of the recv really means somthing to us so
 	we extract that. 
 
 */
 
-/*
 void get_recv_data() {
 	uint8_t raw[30];
 	
 	while (true) {
 		uint8_t val;
 		
-		while (!Serial.available());
+		while (Serial.available() < 32);
+		
 		val = Serial.read();
 		if (val != 0x20) continue;
 		
-		while (!Serial.available());
 		val = Serial.read();
 		if (val != 0x40) continue;
 		
 		for (uint8_t idx = 0; idx < 30; ++idx) {
-			while (!Serial.available());
 			raw[idx] = Serial.read();
 		}
 		
-		data[14] = raw[28]  + (raw[29] << 8);
+		recv[14] = raw[28]  + (raw[29] << 8);
 		
 		uint16_t chksum = 0xFFFF - 0x20 - 0x40;
 		for (uint8_t i = 0; i < 28; i++) {
 			chksum -= raw[i];
 		}
 		
-		if (data[14] == chksum) break;
-		else continue; // Error found in the data
+		if (recv[14] == chksum) break;
+		else continue; // Error found in the recv
 	}
 	
-	data[ 0] = raw[ 0] + (raw[ 1] << 8);
-	data[ 1] = raw[ 2] + (raw[ 3] << 8);
-	data[ 2] = raw[ 4] + (raw[ 5] << 8);
-	data[ 3] = raw[ 6] + (raw[ 7] << 8);
-	data[ 4] = raw[ 8] + (raw[ 9] << 8);
-	data[ 5] = raw[10] + (raw[11] << 8);
-	data[ 6] = raw[12] + (raw[13] << 8);
-	data[ 7] = raw[14] + (raw[15] << 8);
-	data[ 8] = raw[16] + (raw[17] << 8);
-	data[ 9] = raw[18] + (raw[19] << 8);
-	data[10] = raw[20] + (raw[21] << 8); // 1. These 4 are dummy channels that we dont care about
-	data[11] = raw[22] + (raw[23] << 8); // 2. 
-	data[12] = raw[24] + (raw[25] << 8); // 3. 
-	data[13] = raw[26] + (raw[27] << 8); // 4. 
+	recv[ 0] = raw[ 0] + (raw[ 1] << 8);
+	recv[ 1] = raw[ 2] + (raw[ 3] << 8);
+	recv[ 2] = raw[ 4] + (raw[ 5] << 8);
+	recv[ 3] = raw[ 6] + (raw[ 7] << 8);
+	recv[ 4] = raw[ 8] + (raw[ 9] << 8);
+	recv[ 5] = raw[10] + (raw[11] << 8);
+	recv[ 6] = raw[12] + (raw[13] << 8);
+	recv[ 7] = raw[14] + (raw[15] << 8);
+	recv[ 8] = raw[16] + (raw[17] << 8);
+	recv[ 9] = raw[18] + (raw[19] << 8);
+	recv[10] = raw[20] + (raw[21] << 8); // 1. dummy channels
+	recv[11] = raw[22] + (raw[23] << 8); // 2. 
+	recv[12] = raw[24] + (raw[25] << 8); // 3. 
+	recv[13] = raw[26] + (raw[27] << 8); // 4. 
 }
-
-*/
