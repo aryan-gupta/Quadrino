7baf52704f55f09f2d19215ddaf9f9b4223a6858
Aryan Gupta <kuantum.freak@gmail.com>
2018-07-06 20:41:48 -0400

    Move PID function

--- FlightCtrl/FlightCtrl.ino
+++ FlightCtrl/FlightCtrl.ino
@@ -49,10 +49,90 @@ void loop() {
 	/// ================ PHASE 2 =============================
 	s = TCNT1;
 		output_esc_pulse(ESC_BL_DOWN, escbl, ESC_BR_DOWN, escbr);
-			// update_pid_calc();
+			//update_pid_calc();
 		finish_esc_pulse();
 	e = s + PHASE3_TICKS;
 	while (TCNT1 < e);
 	
-	loop_elapsed = (TCNT1 - loop_start) / 2000000.0;
+	loop_elapsed = TCNT1 / 2000000.0;
+}
+
+// Im keeping this here now, will be moved later
+void update_pid_calc() {
+	// the recv_ch? calues are between 2000 and 4000
+	// where 3000 is the center stick value
+	// Channel1 = Roll
+	// Channel2 = Pitch
+	// Channel3 = Yaw
+	// Channel4 = Throttle
+	// The total region we want to map is from -40 degrees below the axis
+	// and 40 degrees above the axis. this is 80 degrees we need to map 
+	// to the value from -1000 to 1000 
+	// 80 / 2000 = 0.04 this means that each tick of the reciver tick is equal
+	// to 0.04 degrees of tilt
+	
+	float ch_value, error, pid_p, pid_d;
+	const float ANGLE_FACTOR = 0.02;
+	
+	// ROLL
+	ch_value = recv[ROLL] - 1500; // map the values from -500 to 500
+	ch_value *= ANGLE_FACTOR; // this will give us the degrees that the reciver should get
+	
+	error = anglex - ch_value;
+	
+	pid_p = KP * error;
+	pid_i_roll += KI * error;
+	pid_d = KD * (error - prev_roll_error)/loop_elapsed;
+	
+	pid_roll = pid_p + pid_i_roll + pid_d;
+	
+	prev_roll_error = error;
+	
+	// PITCH
+	ch_value = recv[PITCH] - 1500;
+	ch_value *= ANGLE_FACTOR;
+	
+	error = angley - ch_value;
+	
+	pid_p = KP * error;
+	pid_i_pitch += KI * error;
+	pid_d = KD * (error - prev_pitch_error)/loop_elapsed;
+	
+	pid_pitch = pid_p + pid_i_pitch + pid_d;
+	
+	prev_pitch_error = error;
+	
+	// YAW
+	ch_value = recv[YAW] - 1500;
+	ch_value *= ANGLE_FACTOR;
+	
+	error = anglez - ch_value;
+	
+	pid_p = KP * error;
+	pid_i_yaw += KI * error;
+	pid_d = KD * (error - prev_yaw_error)/loop_elapsed;
+	
+	pid_yaw = pid_p + pid_i_yaw + pid_d;
+	
+	prev_yaw_error = error;
+	
+	// Final
+	uint16_t throttle = recv[THROTTLE] * 2;
+	
+	escfr = throttle + pid_roll - pid_pitch - pid_yaw; // (front right CCW)
+	escfl = throttle - pid_roll - pid_pitch + pid_yaw; // (font left CW)
+	escbr = throttle + pid_roll + pid_pitch + pid_yaw; // (back right CW)
+	escbl = throttle - pid_roll + pid_pitch - pid_yaw; // (back left CCW)
+	
+	if      (escfr < 2000) escfr = 2000;
+	else if (escfr > 4000) escfr = 4000;
+	
+	if      (escfl < 2000) escfl = 2000;
+	else if (escfl > 4000) escfl = 4000;
+	
+	if      (escbr < 2000) escbr = 2000;
+	else if (escbr > 4000) escbr = 4000;
+	
+	if      (escbl < 2000) escbl = 2000;
+	else if (escbl > 4000) escbl = 4000;
 }
